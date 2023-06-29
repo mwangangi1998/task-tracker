8e439168919a4744915f040908e7c8ceeabccd68
mwangangimusyoka1998@gmail.com <mwangangimusyoka1998@gmail.com>
2023-06-29 16:22:15 +0300

    working on add form validation

--- src/components/AddTask.vue
+++ src/components/AddTask.vue
@@ -1,13 +1,13 @@
 <template>
-    <form class="add-form">
+    <form class="add-form" @submit="onSub()">
         <div class="form-group">
             <input type="text" id="text" v-model="text"  placeholder="Add Task" >
         </div>
         <div class="form-group">
             <input type="text" id="day"  v-model="day" placeholder="Add Day and time" >
         </div>
-        <div class="form-group ">
-           <Label> set reminder<input type="checkbox"  id="reminder" v-model="reminder"  placeholder="Add Day and time" ></Label>
+        <div class="form-group xos">
+           <Label > set reminder</Label><input type="checkbox"  id="reminder" v-model="reminder"  placeholder="Add Day and time" >
         </div>
         <div class="form-group">
             <input type="submit" class="btn" >
@@ -28,6 +28,15 @@ export default{
             reminder:false,
         }
     },
+    methods: {
+        onSub(e){
+            e.preventDefault()
+            if (!this.text ) {
+                alert('Add Task')
+                return
+            }
+        }
+    },
 }
 
 </script>
@@ -38,9 +47,17 @@ input{
    color: rgb(4, 1, 1);
     
 }
+Label{
+    
+    font-weight: bolder; 
+   text-transform: capitalize;
+}
 .btn{
     background: #2040cf;
     color:white;
+}
+#reminder{
+    height: 23px;
 }
 .form-group{
     padding-top: 20px;
